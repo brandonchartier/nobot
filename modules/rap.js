87301a602b7c8e7260db457bcfd4d9f6218f12d7
Simon Brydon <simonbrydon@gmail.com>
2016-01-24 17:49:40 +0100

    remove rapgenius-js dependency, switch to musixmatch REST API

--- modules/rap.js
+++ modules/rap.js
@@ -1,28 +1,54 @@
 var _ = require("lodash");
 var config = require("../config");
-var rapgenius = require("rapgenius-js");
+var request = require("request");
 
 var regex = new RegExp("^" + config.nick + "[^\\s]*\\s+(?:rap|sing)\\s(?:about\\s)?(.+)", "i");
 
 function makeRequest(query, done) {
-  function lyricsSearchDone(err, lyrics) {
+  function trackSearchDone(err, res, body) {
     if (err) return done(err);
 
-    var lines = lyrics.getFullLyrics(false).split("\n");
-    var joined = _.compact(lines).slice(0, 3).join(" / ");
+    var tracks = body.message.body.track_list;
+    if (!tracks) return done(null, "Rap not found", true);
 
-    if (joined.length > 420) return done(null, "Nazty rap", true);
+    var lyricsParams = {
+      uri: "http://api.musixmatch.com/ws/1.1/track.lyrics.get",
+      json: true,
+      qs: {
+        "track_id": _.sample(tracks).track.track_id,
+        "format": "json",
+        "apikey": config.musix.key
+      }
+    };
+
+    request(lyricsParams, getLyricsDone);
+  }
+
+  function getLyricsDone(err, res, body) {
+    if (err) return done(err);
+
+    var lyrics = body.message.body.lyrics;
+    if (lyrics.restricted == 1) return done(null, "Nazty rap", true);
+
+    var lines = lyrics.lyrics_body.split("\n");
+    var joined = _.compact(lines).slice(0, 4).join(" / ");
     done(null, joined);
   }
 
-  function songSearchDone(err, songs) {
-    if (err) return done(err);
-    if (!songs.length) return done(null, "Rap not found", true);
+  var trackParams = {
+    uri: "http://api.musixmatch.com/ws/1.1/track.search",
+    json: true,
+    qs: {
+      "q_lyrics": query,
+      "page_size": 25,
+      "f_has_lyrics": 1,
+      "f_lyrics_language": "en",
+      "format": "json",
+      "apikey": config.musix.key
+    }
+  };
 
-    rapgenius.searchSongLyrics(_.sample(songs).link, "rap", lyricsSearchDone);
-  }
-
-  rapgenius.searchSong(query, "rap", songSearchDone);
+  request(trackParams, trackSearchDone);
 }
 
 function rap(text, done) {
