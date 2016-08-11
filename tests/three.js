d4cb552a6d2445051935d2db08eacaf3affa9671
Brandon Chartier <brandon.chartier@gmail.com>
2016-08-11 15:58:27 -0400

    fixing three word phrase test

--- tests/three.js
+++ tests/three.js
@@ -3,5 +3,5 @@ import config from '../config';
 import threeWords from '../modules/three';
 
 test.cb('three', t => {
-	threeWords(`${config.nick}: date`, t.end);
+	threeWords(`${config.nick}: three`, t.end);
 });
